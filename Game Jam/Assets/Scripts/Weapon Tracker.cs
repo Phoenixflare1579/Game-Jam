6f7f4cfc4db0230d2a5f6f8e095d6b6c3531f27e
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2024-02-23 14:40:55 -0500

    Added Folders for Weapon Sprites and Updated weapon name

--- Game Jam/Assets/Scripts/Weapon Tracker.cs
+++ Game Jam/Assets/Scripts/Weapon Tracker.cs
@@ -41,7 +41,7 @@ public class WeaponTracker : MonoBehaviour
         }
         else if (player.GetComponent<PlayerStats>().Weapon == 5)
         {
-            this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Fishing Rod";
+            this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Cannon";
             this.GetComponent<Image>().sprite = weapons5[player.GetComponent<PlayerStats>().Upgrade];
         }
     }

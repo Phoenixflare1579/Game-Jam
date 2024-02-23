1af94c4a7727e772355d486355b5b58c91bfcf86
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2024-02-23 14:25:14 -0500

    Updated UI and Implemented new sprites

--- Game Jam/Assets/Scripts/Weapon Tracker.cs
+++ Game Jam/Assets/Scripts/Weapon Tracker.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 public class WeaponTracker : MonoBehaviour
 {
     GameObject player;
-    Image [] weapons1;
-    Image[] weapons2;
-    Image[] weapons3;
-    Image[] weapons4;
-    Image[] weapons5;
+    public Sprite[] weapons1;
+    public Sprite[] weapons2;      
+    public Sprite[] weapons3;
+    public Sprite[] weapons4;
+    public Sprite[] weapons5;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -22,26 +22,27 @@ public class WeaponTracker : MonoBehaviour
         if (player.GetComponent<PlayerStats>().Weapon == 1)
         {
             this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Fishing Rod";
-            if (player.GetComponent<PlayerStats>().Upgrade == 0)
-            {
-
-            }
+            this.GetComponent<Image>().sprite = weapons1[player.GetComponent<PlayerStats>().Upgrade];
         }
         else if (player.GetComponent<PlayerStats>().Weapon == 2)
         {
             this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Fishing Net";
+            this.GetComponent<Image>().sprite = weapons2[player.GetComponent<PlayerStats>().Upgrade];
         }
         else if (player.GetComponent<PlayerStats>().Weapon == 3)
         {
             this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Harpoon Gun";
+            this.GetComponent<Image>().sprite = weapons3[player.GetComponent<PlayerStats>().Upgrade];
         }
         else if (player.GetComponent<PlayerStats>().Weapon == 4)
         {
             this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Fishing Spear";
+            this.GetComponent<Image>().sprite = weapons4[player.GetComponent<PlayerStats>().Upgrade];
         }
         else if (player.GetComponent<PlayerStats>().Weapon == 5)
         {
             this.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Fishing Rod";
+            this.GetComponent<Image>().sprite = weapons5[player.GetComponent<PlayerStats>().Upgrade];
         }
     }
 }

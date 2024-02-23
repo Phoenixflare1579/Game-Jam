3558bf3b13de2acaf268575c46639e8ae6e52ed8
Phoenixflare1579 <114099630+Phoenixflare1579@users.noreply.github.com>
2024-02-22 22:26:24 -0500

    Using new input

---
+++ Game Jam/Assets/Scripts/Player_MovewInput.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_MovewInput : MonoBehaviour
+{
+    public PlayerInput MyInput;
+    public InputActionAsset MyMap;
+    public Rigidbody rb;
+    public float speed = 3f;
+    public Vector2 lastDirection;
+
+    void Start()
+    {
+        MyInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
+        if (MyInput != null)
+        {
+            MyMap = MyInput.actions;
+        }
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector3(lastDirection.x, 0, lastDirection.y).normalized * speed;
+    }
+
+    public void Move(InputAction.CallbackContext c)
+    {
+        if (c.performed)
+        {
+            lastDirection = c.ReadValue<Vector2>();
+        }
+        else if (c.canceled)
+        {
+            lastDirection = Vector2.zero;
+        }
+    }
+}

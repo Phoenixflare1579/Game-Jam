c8ef6eac08feed56bde553b5a34ba694acb4688f
Tspangler1234 <tspangler1234@gmail.com>
2024-02-24 12:59:06 -0500

    Weapon Level Up menu
    Started weapon level up menu logic

---
+++ Game Jam/Assets/Scripts/WeaponLevelUpMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class WeaponLevelUp : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void upgradeWeapon()
+    {
+
+    }
+
+    public void changeWeapon()
+    {
+
+    }
+
+    public void revertWeapon()
+    {
+     
+
+    }
+
+}

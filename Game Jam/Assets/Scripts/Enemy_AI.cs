c750048099cc77a7a0e3332a84f67126afe19570
Ago0495 <116462335+Ago0495@users.noreply.github.com>
2024-02-23 20:27:54 -0500

    Enemy hit detection and player damage taken

--- Game Jam/Assets/Scripts/Enemy_AI.cs
+++ Game Jam/Assets/Scripts/Enemy_AI.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class Enemy_AI : MonoBehaviour
 {
     public Rigidbody rb;
-    private BoxCollider Ecollider;
+    //private BoxCollider Ecollider;
     public float speed = 2;
     public float dectectionRange;
     public Vector3 direction;
 
     public GameObject target;
 
+    public float attackCooldown = 1;
+    public float attackRange = 1;
+    public int attackDamage = 5;
+
+    private RaycastHit[] hits;
+    private IEnumerator coroutine;
+
     void Start()
     {
         this.transform.rotation = Quaternion.Euler(90, 0, 0);
-        Ecollider = GetComponent<BoxCollider>();
+        //Ecollider = GetComponent<BoxCollider>();
         rb = GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Player");
+
+        coroutine = AttackWait();
+        StartCoroutine(coroutine);
     }
 
 
@@ -27,14 +37,35 @@ public class Enemy_AI : MonoBehaviour
 
         rb.velocity = direction.normalized * speed;
 
-        if (Physics.CheckSphere(transform.position, 2))
+    }
+
+    private IEnumerator AttackWait()
+    {
+        while (true)
         {
-            Attack(target);
-        }
+            hits = Physics.RaycastAll(transform.position, direction, attackRange);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+                GameObject hitObject = hit.collider.gameObject;
+
+                if (hitObject.tag == "Player")
+                {
+                    Attack(target);
+                }
+
+            }
+
+            yield return new WaitForSeconds(attackCooldown);
+
+    }
     }
 
     void Attack(GameObject target)
     {
+        PlayerStats targetStats = target.GetComponent<PlayerStats>();
         Debug.Log("hit target");
+        targetStats.HP -= attackDamage;
     }
 }
